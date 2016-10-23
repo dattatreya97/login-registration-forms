f8df287db3c3ea64aafcf06615de57d1dcca58b4
Dattatreya Nayak <dattu4621@gmail.com>
2016-10-23 09:19:33 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "Dattu@3121";
-$db = "EduTrip";
+$pass = "";
+$db = "Database_name";
 
 $y=mysqli_connect($host, $user, $pass,$db);
 
@@ -11,14 +11,14 @@ $y=mysqli_connect($host, $user, $pass,$db);
 if(isset($_POST['username'])){
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-    $sql = "SELECT * FROM users WHERE username = '".$username."' AND password = '".$password."' LIMIT 1";
+    $sql = "SELECT * FROM TABLENAME WHERE username = '".$username."' AND password = '".$password."' LIMIT 1";
 		$res = mysqli_query($y,$sql);
 		$x =mysqli_num_rows($res);
 		if( $x==1){
-			header('location: http://127.0.0.1/EduTrip/userinput.html');
+			header('location: http://');//desired location
 		}
 		else {
-			header('location: http://127.0.0.1/EduTrip/loginerror.html');
+			header('location: http://');//desired location
 		}
 }
 ?>
